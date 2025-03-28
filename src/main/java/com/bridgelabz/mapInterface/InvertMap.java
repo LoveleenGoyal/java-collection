2d3de38a8f0153bd32a0042106b9379b869aba49
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-28 22:16:52 +0530

    [Loveleen] ADD: Invert a map

---
+++ src/main/java/com/bridgelabz/mapInterface/InvertMap.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.mapInterface;
+
+import java.util.*;
+
+public class InvertMap {
+    public static void main(String[] args) {
+        Map<String, Integer> originalMap = new HashMap<>();
+        originalMap.put("A", 1);
+        originalMap.put("B", 2);
+        originalMap.put("C", 1);
+
+        Map<Integer, List<String>> invertedMap = invertMap(originalMap);
+        System.out.println("Inverted Map: " + invertedMap);
+    }
+
+    public static <K, V> Map<V, List<K>> invertMap(Map<K, V> map) {
+        Map<V, List<K>> inverted = new HashMap<>();
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            inverted.computeIfAbsent(entry.getValue(), k -> new ArrayList<>()).add(entry.getKey());
+        }
+        return inverted;
+    }
+}
+
+/*Inverted Map: {1=[A, C], 2=[B]}*/
