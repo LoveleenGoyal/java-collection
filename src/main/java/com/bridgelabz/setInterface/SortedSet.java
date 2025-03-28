a18c9b3b14a665ae7759bb563033e80b207e5cdb
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-28 15:21:14 +0530

    [Loveleen] ADD: Elements present in either set

--- src/main/java/com/bridgelabz/setInterface/SortedSet.java
+++ src/main/java/com/bridgelabz/setInterface/SortedSet.java
@@ -1,2 +1,15 @@
-package com.bridgelabz.setInterface;public class SortedSet {
+package com.bridgelabz.setInterface;
+
+import java.util.*;
+
+public class SortedSet {
+    public static void main(String[] args) {
+        HashSet<Integer> elements = new HashSet<>(Arrays.asList(5,3,9,1));
+
+        System.out.println(elements+ " sorted list " + convertToSortedList(elements));
+    }
+
+    public static List<Integer> convertToSortedList(Set<Integer> set) {
+        return new ArrayList<>(set);
+    }
 }
