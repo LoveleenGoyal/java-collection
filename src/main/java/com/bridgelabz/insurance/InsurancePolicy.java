ff35dbc095cae7b0b8b1e7370ef0915b496b21e5
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-03-28 21:33:20 +0530

    [Loveleen] ADD: Insurance policy

--- src/main/java/com/bridgelabz/insurance/InsurancePolicy.java
+++ src/main/java/com/bridgelabz/insurance/InsurancePolicy.java
@@ -1,4 +1,55 @@
 package com.bridgelabz.insurance;
 
-public class InsurancePolicy {
+import java.util.Date;
+import java.util.Objects;
+
+class InsurancePolicy implements Comparable<InsurancePolicy> {
+    private String policyNumber;
+    private String policyholderName;
+    private Date expiryDate;
+    private String coverageType;
+    private double premiumAmount;
+
+    public InsurancePolicy(String policyNumber, String policyholderName, Date expiryDate, String coverageType, double premiumAmount) {
+        this.policyNumber = policyNumber;
+        this.policyholderName = policyholderName;
+        this.expiryDate = expiryDate;
+        this.coverageType = coverageType;
+        this.premiumAmount = premiumAmount;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public String getCoverageType() {
+        return coverageType;
+    }
+
+    @Override
+    public int compareTo(InsurancePolicy other) {
+        return this.expiryDate.compareTo(other.expiryDate);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof InsurancePolicy)) return false;
+        InsurancePolicy that = (InsurancePolicy) obj;
+        return policyNumber.equals(that.policyNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(policyNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "PolicyNumber: " + policyNumber + ", Holder: " + policyholderName + ", Expiry: " + expiryDate + ", Coverage: " + coverageType + ", Premium: " + premiumAmount;
+    }
 }
